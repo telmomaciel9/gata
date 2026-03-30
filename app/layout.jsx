a010c61087a86b64176b78a2634a079218c34874
Telmo Maciel <telmomaciel9@gmail.com>
2026-03-30 23:39:58 +0100

    Template atualizado

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Gata na Praia 2026',
+  description: 'Resultados em tempo real',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt">
+      <body>{children}</body>
+    </html>
+  )
+}
